a81b8ea88cefbe4193008a0594ea85208ee09c75
Lskrzypc <louis.skrzypczak@student.junia.com>
2025-02-04 22:42:08 +0100

    fix(auth): Log validation errors instead of throwing bad request at the moment

--- src/modules/auth/controllers/register-controller.ts
+++ src/modules/auth/controllers/register-controller.ts
@@ -2,14 +2,12 @@ import type { Request, Response } from 'express';
 import type { CreateUserHttpRequest, CreateUserHttpResponse } from '../model/auth-model';
 import { createUserSchema } from '../model/auth-model';
 import { registerUseCase } from '../use-cases/register-use-case';
-import type { IErrorMessage } from '../../../core/errors/errors';
-import { badRequestError } from '../../../core/errors/errors';
 
 const registerController = {
-  register: async (req: Request<CreateUserHttpRequest>, res: Response<CreateUserHttpResponse | IErrorMessage>) => {
+  register: async (req: Request<CreateUserHttpRequest>, res: Response<CreateUserHttpResponse>) => {
     const { error } = createUserSchema.validate(req.body);
     if (error) {
-      throw badRequestError(error.message);
+      console.error(error);
     }
 
     const pendingUser = req.body;

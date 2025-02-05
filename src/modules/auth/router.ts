8318b8fc727c9f617e8323550a459701d5b62652
Lskrzypc <louis.skrzypczak@student.junia.com>
2025-02-05 10:35:28 +0100

    feat(auth): Add login route and implement login use case with improved password handling

--- src/modules/auth/router.ts
+++ src/modules/auth/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { registerController } from './controllers/register-controller';
+import { loginController } from './controllers/login-controller';
 
 const authenticationRouter: Router = Router();
 
 authenticationRouter.post('/signup', registerController.register);
+authenticationRouter.post('/login', loginController.login);
 
 export default authenticationRouter;
--- src/modules/auth/use-cases/login-use-case.ts
+++ src/modules/auth/use-cases/login-use-case.ts
@@ -4,28 +4,31 @@ import User from '../../users/repository/user-repository';
 import type { AuthenticateUserHttpRequest, AuthenticateUserHttpResponse } from '../model/auth-model';
 
 const loginUseCase = async (data: AuthenticateUserHttpRequest): Promise<AuthenticateUserHttpResponse> => {
-  const authenticationData = data;
+  const givenAuthenticationData = data;
 
-  const email = authenticationData.email ?? '';
-  const password = authenticationData.password ?? '';
-
-  const user = await User.findOne({ email: email });
+  const user = await User.findOne({ email: givenAuthenticationData.email });
 
   if (!user) {
     throw new Error('User not found');
   }
 
-  const isPasswordCorrect = await compare(password, user.password);
+  const actualUser = {
+    _id: user._id,
+    email: user.email,
+    password: user.password,
+  };
+
+  const isPasswordCorrect = await compare(givenAuthenticationData.password ?? '', actualUser.password);
 
   if (!isPasswordCorrect) {
     throw new Error('Incorrect password');
   }
 
-  const accessToken = sign({ email: user.email }, process.env.JWT_SECRET ?? '', {
-    expiresIn: '1h',
+  const accessToken = sign({ email: actualUser.email }, process.env.JWT_SECRET ?? '', {
+    expiresIn: '365d',
   });
 
-  return { accessToken, _id: user._id };
+  return { accessToken, _id: actualUser._id.toString() };
 };
 
 export { loginUseCase };
